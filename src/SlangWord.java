2289b76ebd6f2eff90c77900a9660220bdcd9170
unknown <NgocNguyenC3@gmail.com>
2022-03-23 09:44:40 +0700

    change

--- src/SlangWord.java
+++ src/SlangWord.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 public class SlangWord {
     private static String fileDataPath = "slang.txt";
@@ -16,16 +19,75 @@ public class SlangWord {
 
 
     public static void main(String args[]) {
-        getDataFromRootFile();
+        File fileSlangWord = new File(fileDataSlangPackUpPath);
+        File fileDefinition = new File(fileDataDefinitionPackUpPath);
+
+        // Get Data
+        if(!fileSlangWord.exists() || !fileDefinition.exists()) {
+            getDataFromRootFile();
+        } else {
+            getDataBackUp();
+        }
 
         menu();
     }
 
+    // Get My Data
+    private static void getDataBackUp() {
+        listSlang = new HashMap<String, String>();
+        listDefinition = new HashMap<String, Set<String>>();
+
+        //slang_back_up.txt (true) or definition.txt(false)
+        getDataFile(true);
+        getDataFile(false);
+    }
+
+    // Function get Data from slang_back_up.txt (true) or definition.txt(false)
+    private static void getDataFile(Boolean isSlangWord) {
+        FileInputStream fileInputStream = null;
+        BufferedReader bufferedReader = null;
+        try {
+            fileInputStream = new FileInputStream(isSlangWord? fileDataSlangPackUpPath:fileDataDefinitionPackUpPath);
+            bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+            String line ="";
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] value = line.split("`");
+                if(isSlangWord) {
+                    listSlang.put(value[0], value[1]);
+                } else {
+                    if(listDefinition.get(value[0]) == null) {
+                        listDefinition.put(value[0], new HashSet<>());
+                    }
+                    String[] data = value[1].split("\\|");
+                    for (String i: data) {
+                        listDefinition.get(value[0]).add(i);
+                    }
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    // Main menu
     private static void menu() {
         int option = 0;
         do{
             menuOption();
-            option = Integer.parseInt(scanner.nextLine());
+            String ref = scanner.nextLine();
+            try {
+                option = Integer.parseInt(ref);
+            } catch (Exception ex) {
+                printLn("Wrong option");
+                continue;
+            }
+
             if(option < 0 || option > 12) {
                 printLn("Wrong option!");
                 continue;
@@ -40,21 +102,21 @@ public class SlangWord {
                 case 2:
                     eventSearchDefinition();
                     break;
-                //
+                // History slang word search
                 case 3:
-
+                    historySearch();
                     break;
-                //
+                // Add new Slang Word
                 case 4:
-
+                    addNewWord();
                     break;
-                //
+                // Edit Slang Word
                 case 5:
-
+                    editSlangWord();
                     break;
-                //
+                // Delete slang word
                 case 6:
-
+                    deleteData();
                     break;
                 // Reset Data
                 case 7:
@@ -69,18 +131,199 @@ public class SlangWord {
                 case 9:
                     gameStart(true);
                     break;
-                //Funny game random Definition
+                // Funny game Definition random
                 // Mode false: Definition
                 case 10:
                     gameStart(false);
                     break;
-                //
+                //Save Data
                 case 11:
-
+                    saveAllData();
                     break;
             }
         }while(option != 12);
 
+    }
+
+    // Delete Data - Map<SlangWord, def> and Map <Def, Set<SlangWord>>
+    private static void deleteData() {
+        print("Input: ");
+        String slangInput = scanner.nextLine();
+        String output = listSlang.get(slangInput);
+        if(output == null) {
+            printLn("Cant find " + slangInput);
+            return;
+        }
+        //Map <Def, Set<SlangWord>>
+        deleteDefinition(slangInput, output);
+        //Map<SlangWord, def>
+        listSlang.remove(slangInput);
+
+        printLn("Successfully");
+    }
+
+    // Edit data
+    private static void editSlangWord() {
+        print("Input: ");
+        String slangInput = scanner.nextLine();
+        String output = listSlang.get(slangInput);
+
+        if(output == null) {
+            printLn("Cant find " + slangInput);
+            return;
+        }
+        print("Definition: ");
+        String def = scanner.nextLine();
+
+        // Delete old data Map <Def, Set<SlangWord>>
+        deleteDefinition(slangInput, output);
+        // Save new data Map <Def, Set<SlangWord>>
+        saveEditDefinition(slangInput, def);
+        listSlang.put(slangInput, def);
+
+        printLn("Successfully");
+    }
+
+    // Save new data Map <Def, Set<SlangWord>>
+    private static void saveEditDefinition(String slangInput, String def) {
+        String[] data = def.split("\\|");
+        for(String i: data) {
+            Set<String> set = listDefinition.get(i.toUpperCase());
+            if(set == null) {
+                listDefinition.put(i.toUpperCase(), new HashSet<>());
+            }
+
+            listDefinition.get(i.toUpperCase()).add(slangInput);
+        }
+    }
+
+    // Delete data Map <Def, Set<SlangWord>>
+    private static void deleteDefinition(String input, String output) {
+        String[] data = output.split("\\|");
+
+        for(String i: data) {
+            Set<String> set = listDefinition.get(i.toUpperCase());
+            if(set != null) {
+                set.remove(input);
+                if(set.size() == 0) listDefinition.remove(i.toUpperCase());
+            }
+        }
+
+
+    }
+
+    // Write file
+    private static void saveAllData() {
+        saveSlangWord();
+        saveDefinitionWord();
+    }
+
+    // Write definition.txt
+    private static void saveDefinitionWord() {
+        File f = new File(fileDataDefinitionPackUpPath);
+        if(!f.exists()) {
+            try {
+                f.createNewFile();
+            } catch (IOException e) {
+                printLn("Cant create history file");
+                return;
+            }
+        }
+
+        try {
+            FileWriter fw = new FileWriter(f);
+            Set<String> setKey = listDefinition.keySet();
+            for (String key: setKey) {
+                String line = "";
+                Set<String> data = listDefinition.get(key);
+                for(String i: data) {
+                    line += i + "|";
+                }
+                if(line == "") continue;
+                fw.write(key + "`" + line + "\n");
+            }
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        printLn("Successfully");
+    }
+
+    // Write slang_back_up.txt
+    private static void saveSlangWord() {
+        File f = new File(fileDataSlangPackUpPath);
+        if(!f.exists()) {
+            try {
+                f.createNewFile();
+            } catch (IOException e) {
+                printLn("Cant create history file");
+                return;
+            }
+        }
+
+        try {
+            FileWriter fw = new FileWriter(f);
+            Set<String> setKey = listSlang.keySet();
+            for (String key: setKey) {
+                fw.write(key + "`" + listSlang.get(key) + "\n");
+            }
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // Add new slang word
+    private static void addNewWord() {
+        print("Slang Word: ");
+        String key = scanner.nextLine();
+        print("Definition: ");
+        String data = scanner.nextLine();
+        listSlang.put(key,data);
+
+        // Create new Def
+        if(data.contains("|")) {
+            String[] value = data.split("\\|");
+
+            for(String i: value) {
+                Set<String> set = listDefinition.get(i.toUpperCase());
+                if(set == null) {
+                    listDefinition.put(i.toUpperCase(), new HashSet<>());
+
+                }
+                listDefinition.get(i.toUpperCase(Locale.ROOT)).add(key);
+            }
+        }
+
+        printLn("Successfully");
+    }
+
+    // History search slang word, Slangword - time
+    private static void historySearch() {
+        List<String> listHistory = new ArrayList<>();
+        FileInputStream fileInputStream = null;
+        BufferedReader bufferedReader = null;
+
+        try {
+            fileInputStream = new FileInputStream(fileHistory);
+            bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+            String line ="";
+            while((line = bufferedReader.readLine()) != null) {
+                if(line.equals("")) continue;
+                listHistory.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            printLn("No history");
+            return;
+        } catch (IOException e) {
+            return;
+        }
+
+        for(String key: listHistory) {
+            printLn(key);
+        }
     }
 
     // Mode true: Slang Word, Mode false: Definition
@@ -153,14 +396,44 @@ public class SlangWord {
     private static void eventSearchSlangWord() {
         print("Input: ");
         String slangInput = scanner.nextLine();
+        scanner.useDelimiter("\n");
         startTime = System.nanoTime();
         String output = listSlang.get(slangInput);
+        scanner.useDelimiter("\n");
         endTime = System.nanoTime();
 
         printResultSlangWord(slangInput, output, startTime, endTime);
 
+        saveHistorySlangWord(slangInput);
     }
 
+    // Save History Slang Word
+    private static void saveHistorySlangWord(String input) {
+        java.util.Date date = new java.util.Date();
+        String time = date.toString();
+
+        File f = new File(fileHistory);
+        if(!f.exists()) {
+            try {
+                f.createNewFile();
+                FileWriter fw = new FileWriter(f);
+                fw.write("Slang Word          Time"+ "\n" );
+                fw.close();
+            } catch (IOException e) {
+                printLn("Cant create history file");
+                return;
+            }
+        }
+
+        try {
+            FileWriter fw = new FileWriter(f, true);
+            fw.write(input + "      " + time + "\n" );
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
     // Show menu
     private static void menuOption() {
         printLn("");
@@ -170,8 +443,8 @@ public class SlangWord {
         printLn("| 1. Search by Slang Word                                        |");
         printLn("| 2. Search by Definition                                        |");
         printLn("| 3. History search                                              |");
-        printLn("| 4. Add new Search                                              |");
-        printLn("| 5. Edit slang word                                             |");
+        printLn("| 4. Add new Slang Word                                          |");
+        printLn("| 5. Edit Slang Word                                             |");
         printLn("| 6. Delete slang word                                           |");
         printLn("| 7. Reset Data                                                  |");
         printLn("| 8. Random slang word                                           |");
@@ -279,5 +552,5 @@ public class SlangWord {
         System.out.println("Slang Word:" + output);
         printLn("Total execution time: "+ (endTime- startTime) + " nanoseconds");
     }
-    // End Utility
+    // End Print
 }
